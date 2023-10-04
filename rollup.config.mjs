e97c8b65df6347bc1517b4a1ddfefa9b592f4a5f
Bartosz <bartek624@gmail.com>
2023-10-04 20:50:26 +0200

    Reconfigured with postcss

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { babel } from '@rollup/plugin-babel';
-import scss from 'rollup-plugin-scss';
+import postcss from 'rollup-plugin-postcss'
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -22,14 +22,14 @@ export default [
       },
     ],
     plugins: [
+    	peerDepsExternal(),
+    	resolve(),
         commonjs(),
         babel({
             exclude: 'node_modules/**',
             presets: ['@babel/preset-react']
         }),
-        scss({ fileName: 'bundle.css' }),
-        peerDepsExternal(),
-        resolve(),
+        postcss()
     ],
   }
 ];
